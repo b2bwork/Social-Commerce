42e9f2ed1a6f8820d1f301db9df860df2bfda95a
lagmanzaza <lagmanmc@gmail.com>
2017-07-23 19:16:05 +0700

    [Angular] add user's address form component

--- client/src/app/Components/user-profile/user-address/user-address.component.ts
+++ client/src/app/Components/user-profile/user-address/user-address.component.ts
@@ -1,3 +1,4 @@
+import { Apollo } from 'apollo-angular';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserAddressComponent implements OnInit {
 
-  constructor() { }
+  constructor(private apollo:Apollo) { }
 
   ngOnInit() {
   }
