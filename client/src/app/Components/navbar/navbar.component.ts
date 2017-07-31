614a2fad2bc4609b028bdcb614a3ded2e62a6734
lagmanzaza <lagmanmc@gmail.com>
2017-07-31 20:33:33 +0700

    [Angular , Apollo , Bootstrap3] add navbar component for user login add user profile link

--- client/src/app/Components/navbar/navbar.component.ts
+++ client/src/app/Components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Apollo } from 'apollo-angular';
+import { Component, OnInit, ChangeDetectorRef, } from '@angular/core';
+import gql from 'graphql-tag';
 
 @Component({
   selector: 'navbar',
@@ -7,8 +10,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  userImageQuery = gql`
+    query image($UserID: String!){
+      userProfile(UserID: $UserID){
+        profileImage
+      }
+    }
+  `;
+  userData: any;
+  userAuth: boolean = false;
+  userImage: string ;
+  constructor(private apollo: Apollo ,private cdRef:ChangeDetectorRef , private router: Router) { 
+    setTimeout(()=>{
+      if(localStorage.getItem('userID') != null && localStorage.getItem('provider') == 'Google'){
+        apollo.watchQuery({query: this.userImageQuery , variables:{
+          UserID: localStorage.getItem('userID') ,
+        }}).subscribe(({data , loading})=>{
+          this.userAuth = true;
+          this.userData = data;
+          let {userProfile} = this.userData;
+          let image = userProfile.profileImage;
+          this.userImage = image.replace('sz=200','sz=40');
+          cdRef.detectChanges();
+        })
+    }else if(localStorage.getItem('userID') != null && localStorage.getItem('provider') == 'Facebook'){
+      apollo.watchQuery({query: this.userImageQuery , variables:{
+        UserID: localStorage.getItem('userID') ,
+      }}).subscribe(({data , loading})=>{
+        this.userAuth = true;
+        this.userData = data;
+        let {userProfile} = this.userData;
+        this.userImage = userProfile.profileImage;
+        cdRef.detectChanges();
+      })
+    }else{
+      router.navigate(['/']);
+    }
+    },500);    
+  }
 
+  logout(){
+    localStorage.removeItem('userID');
+    localStorage.removeItem('provider');
+    localStorage.removeItem('profileImage');
+  }
+  
   ngOnInit() {
   }
 
