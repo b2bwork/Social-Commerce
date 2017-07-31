2871ee91532a756f1a1f76198c528c0205a7dc00
lagmanzaza <lagmanmc@gmail.com>
2017-07-31 20:30:51 +0700

    [Angular , Apollo ,Boostrap3] add component for list categorys

--- client/src/app/Components/listcategorys/listcategorys.component.ts
+++ client/src/app/Components/listcategorys/listcategorys.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Apollo ,ApolloQueryObservable} from 'apollo-angular';
+import { Apollo, ApolloQueryObservable } from 'apollo-angular';
 import gql from 'graphql-tag';
 import 'rxjs/add/operator/map';
 
@@ -10,26 +10,43 @@ import 'rxjs/add/operator/map';
 })
 export class ListcategorysComponent implements OnInit {
 
- listCategorysQuery = gql`
-    query {
-     listCategory{
-         Name
-         Image
-     }
+  listCategorysQuery = gql`
+    query{
+      liscategory{
+        _id
+        name
+        coverImage
+      }
 }
 `;
-  listCategorys: any;
+  data: any;
   loading: boolean;
-   
-  constructor(private list: Apollo) { 
-
+  listcategorys: any;
+  loged: boolean = false;
+  constructor(private list: Apollo) {
+    this.listCategory();
+  }
+  listCategory() {
+    if (localStorage.getItem('userID') != null && localStorage.getItem('provider') != null) {
+      this.list.watchQuery({ query: this.listCategorysQuery }).subscribe(({ data, loading }) => {
+        this.data = data;
+        let { liscategory } = this.data;
+        this.listcategorys = liscategory;
+        this.loading = loading
+        this.loged = true
+      })
+    } else {
+      this.list.watchQuery({ query: this.listCategorysQuery }).subscribe(({ data, loading }) => {
+        this.data = data;
+        let { liscategory } = this.data;
+        this.listcategorys = liscategory;
+        this.loading = loading
+        this.loged = false
+      })
+    }
   }
 
   ngOnInit() {
-    this.list.watchQuery({ query: this.listCategorysQuery }).subscribe(({data, loading})=>{
-      this.listCategorys = data;
-      this.loading = loading
-    })
   }
 
 }
