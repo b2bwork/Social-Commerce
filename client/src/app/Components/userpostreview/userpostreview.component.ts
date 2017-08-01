6143ea964cf21ccf8c7c792be184089fc00b110e
lagmanzaza <lagmanmc@gmail.com>
2017-08-02 01:25:35 +0700

    [Angular , Apollo , ngx_uploader , froala editor] upload review's cover image and config toolbar  editor

--- client/src/app/Components/userpostreview/userpostreview.component.ts
+++ client/src/app/Components/userpostreview/userpostreview.component.ts
@@ -1,7 +1,8 @@
 import { Router } from '@angular/router';
 import { Apollo } from 'apollo-angular';
 import gql from 'graphql-tag';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
+import { UploadOutput, UploadInput, UploadFile, humanizeBytes } from 'ngx-uploader';
 
 @Component({
   selector: 'app-userpostreview',
@@ -10,10 +11,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserpostreviewComponent implements OnInit {
   froalaConfig: Object = {
-    placeholderText: 'add your review',
     charCounterCount: false,
     imageUploadURL: 'http://localhost:3001/review_image',
     height: 450,
+    toolbarButtons: ['bold', 'italic', 'underline',
+      'fontSize', 'color', '|', 'paragraphFormat', 'align'
+      , 'outdent', 'indent', 'quote', 'insertLink', 'insertImage',],
+    toolbarButtonsSM: ['bold', 'italic', 'underline',
+      'fontSize', 'color', '|', 'paragraphFormat', 'align'
+      , 'outdent', 'indent', 'quote', 'insertLink', 'insertImage',],
+      toolbarButtonsXS: ['bold', 'italic', 'underline',
+      'fontSize', 'color', '|', 'paragraphFormat', 'align'
+      , 'outdent', 'indent', 'quote', 'insertLink', 'insertImage',],
   }
 
   userPostReviewMutation = gql`
@@ -32,6 +41,7 @@ export class UserpostreviewComponent implements OnInit {
                         category: $category ,
                         content: $content ){
                           userID
+                          _id
                         }
     }
   `;
@@ -49,8 +59,17 @@ export class UserpostreviewComponent implements OnInit {
   userReviewCategory: String = '';
   categorys: any;
   posted: String = '';
+  formData: FormData;
+  files: UploadFile[];
+  uploadInput: EventEmitter<UploadInput>;
+  humanizeBytes: Function;
+  dragOver: boolean;
+  previewImage: any;
 
   constructor(private apollo: Apollo, private router: Router) {
+    this.files = [];
+    this.uploadInput = new EventEmitter<UploadInput>();
+    this.humanizeBytes = humanizeBytes;
     this.listCategory();
   }
   listCategory() {
@@ -75,16 +94,54 @@ export class UserpostreviewComponent implements OnInit {
       }).subscribe((data) => {
         let returndata: any = data.data;
         if (returndata.userPostReview.userID == 'success') {
-          this.router.navigate(['/']);
+          this.startUpload(returndata.userPostReview._id);
+          // this.router.navigate(['/']);
         } else if (returndata.userPostReview.userID == 'fail') {
           this.posted = 'เกิดปัญหาการโพส';
         }
       });
-    }else{
+    } else {
       this.posted = 'โปรดกรอกข้อมูลให้ครบ';
     }
   }
 
+  onUploadOutput(output: UploadOutput) {
+    if (output.type === 'allAddedToQueue') {
+    } else if (output.type === 'addedToQueue') {
+
+      this.files.push(output.file);
+
+    } else if (output.type === 'uploading') {
+      const index = this.files.findIndex(file => file.id === output.file.id);
+      this.files[index] = output.file;
+    } else if (output.type === 'removed') {
+      this.files = this.files.filter((file: UploadFile) => file !== output.file);
+    }
+  }
+
+  readUrl(event) {
+    if (event.target.files && event.target.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = (event) => {
+        let data: any = event.target;
+        this.previewImage = data.result;
+      }
+
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  }
+  startUpload(_id): void {
+    const event: UploadInput = {
+      type: 'uploadAll',
+      url: 'http://localhost:3001/review/coverImage',
+      method: 'POST',
+      data: { _id: _id },
+      concurrency: 1
+    }
+
+    this.uploadInput.emit(event);
+  }
   ngOnInit() {
   }
 
