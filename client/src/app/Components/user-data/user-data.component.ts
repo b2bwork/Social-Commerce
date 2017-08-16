7bf382f5a82481bc99cdd8ae341452adc7d8af5f
lagmanzaza <lagmanmc@gmail.com>
2017-08-17 01:05:14 +0700

    [Angular , Apollo] add schema for query user's profile

--- client/src/app/Components/user-data/user-data.component.ts
+++ client/src/app/Components/user-data/user-data.component.ts
@@ -1,19 +1,76 @@
-import gql  from 'graphql-tag';
+import { UserReviewsComponent } from './../user-reviews/user-reviews.component';
+import { UserPollsComponent } from './../user-polls/user-polls.component';
+import { Subscription } from 'apollo-client';
+import { ActivatedRoute } from '@angular/router';
+import gql from 'graphql-tag';
 import { Apollo } from 'apollo-angular';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , OnDestroy } from '@angular/core';
 
 @Component({
-  selector: 'user-data',
+  selector: 'userdata',
   templateUrl: './user-data.component.html',
-  styleUrls: ['./user-data.component.css']
+  styleUrls: ['./user-data.component.css'],
+  entryComponents: [UserPollsComponent , UserReviewsComponent]
 })
-export class UserDataComponent implements OnInit {
+export class UserDataComponent implements OnInit , OnDestroy {
+  sub: Subscription;
   userDataQuery = gql`
-    
+  query profile($UserID:String!){
+    userProfile(UserID:$UserID){
+      profileImage
+      fullName
+      bio
+    }
+    listUserReview(userID:$UserID){
+      _id
+      coverImage
+      title
+    }
+    listUserPollProduct(userID:$UserID){
+      _id
+      products{
+        productName
+        productImage
+        productScore
+      }
+    }
+  }
   `;
-  constructor(private apollo: Apollo) { }
+  loading: Boolean = true;
+  userProfile: any;
+  userPollProducts: any;
+  userReviews: any;
+  userID: String;
+
+  constructor(private apollo: Apollo, private routeParam: ActivatedRoute) {
+    routeParam.params.subscribe((params) => {
+      this.userID = params.userID;
+      this.loaduserProfile(params.userID);
+    });
+  }
+
+  loaduserProfile(userID) {
+    this.sub = this.apollo.watchQuery({
+      query: this.userDataQuery, variables: {
+        UserID: userID
+      }
+    }).subscribe(({data , loading})=>{     
+      let returnData: any = data
+      this.loading = loading;
+      let {userProfile ,listUserPollProduct ,listUserReview} = returnData;
+      this.userPollProducts = listUserPollProduct;
+      this.userReviews = listUserReview;
+      this.userProfile = userProfile;
+    });
+  }
 
   ngOnInit() {
   }
 
+  ngOnDestroy(){
+    this.sub.unsubscribe();
+  }
+
 }
+
+export default UserDataComponent
